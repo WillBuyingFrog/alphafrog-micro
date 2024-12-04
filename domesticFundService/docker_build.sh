0ab711caa16cd7c09bbe5cf9089a24d6e6dfab59
Frog Weng <froggywch@gmail.com>
2024-12-05 00:33:32 +0800

    Add docker config

---
+++ domesticFundService/docker_build.sh
@@ -0,0 +1,3 @@
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+docker build  --build-arg http_proxy=http://127.0.0.1:7890 --build-arg https_proxy=http://127.0.0.1:7890 -t alphafrog-micro-domestic-fund-service:latest ./domesticFundService
