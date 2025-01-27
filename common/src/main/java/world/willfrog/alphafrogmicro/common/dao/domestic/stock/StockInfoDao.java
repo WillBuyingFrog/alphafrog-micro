4d117b5fb2d1db391fc626807cdf95abf5b5b4e8
Frog Weng <froggywch@gmail.com>
2025-01-27 18:43:25 +0800

    fix: unable to match column name and property in StockInfo

--- common/src/main/java/world/willfrog/alphafrogmicro/common/dao/domestic/stock/StockInfoDao.java
+++ common/src/main/java/world/willfrog/alphafrogmicro/common/dao/domestic/stock/StockInfoDao.java
@@ -1,6 +1,8 @@
 package world.willfrog.alphafrogmicro.common.dao.domestic.stock;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import world.willfrog.alphafrogmicro.common.pojo.domestic.stock.StockInfo;
 
@@ -17,6 +19,26 @@ public interface StockInfoDao {
     Integer insertStockInfo(StockInfo stockInfo);
 
     @Select("SELECT * FROM alphafrog_stock_info WHERE ts_code like '%${tsCode}%'")
+    @Results({
+            @Result(column = "id", property = "stockInfoId"),
+            @Result(column = "ts_code", property = "tsCode"),
+            @Result(column = "symbol", property = "symbol"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "area", property = "area"),
+            @Result(column = "industry", property = "industry"),
+            @Result(column = "fullname", property = "fullName"),
+            @Result(column = "enname", property = "enName"),
+            @Result(column = "cnspell", property = "cnspell"),
+            @Result(column = "market", property = "market"),
+            @Result(column = "exchange", property = "exchange"),
+            @Result(column = "curr_type", property = "currType"),
+            @Result(column = "list_status", property = "listStatus"),
+            @Result(column = "list_date", property = "listDate"),
+            @Result(column = "delist_date", property = "delistDate"),
+            @Result(column = "is_hs", property = "isHs"),
+            @Result(column = "act_name", property = "actName"),
+            @Result(column = "act_ent_type", property = "actEntType")
+    })
     List<StockInfo> getStockInfoByTsCode(String tsCode);
 
     @Select("SELECT * FROM alphafrog_stock_info WHERE fullname like '%${fullName}%'")
