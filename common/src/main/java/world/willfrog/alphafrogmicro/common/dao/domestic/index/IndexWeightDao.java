a64019392ddbf615e54adad9543c61203bf9846d
Frog Weng <froggywch@gmail.com>
2025-01-27 17:43:22 +0800

    fix: empty con_code field when executing IndexWeight query

--- common/src/main/java/world/willfrog/alphafrogmicro/common/dao/domestic/index/IndexWeightDao.java
+++ common/src/main/java/world/willfrog/alphafrogmicro/common/dao/domestic/index/IndexWeightDao.java
@@ -1,9 +1,6 @@
 package world.willfrog.alphafrogmicro.common.dao.domestic.index;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import world.willfrog.alphafrogmicro.common.pojo.domestic.index.IndexWeight;
 
 import java.util.List;
@@ -18,6 +15,12 @@ public interface IndexWeightDao {
 
 
     @Select("SELECT * FROM alphafrog_index_weight WHERE index_code = #{tsCode} AND trade_date BETWEEN #{startDate} AND #{endDate}")
+    @Results({
+            @Result(property = "indexCode", column = "index_code"),
+            @Result(property = "conCode", column = "con_code"),
+            @Result(property = "tradeDate", column = "trade_date"),
+            @Result(property = "weight", column = "weight")
+    })
     List<IndexWeight> getIndexWeightsByTsCodeAndDateRange(@Param("tsCode") String tsCode, @Param("startDate") long startDate, @Param("endDate") long endDate);
 
     @Select("SELECT * FROM alphafrog_index_weight WHERE con_code = #{conCode} AND trade_date BETWEEN #{startDate} AND #{endDate}")
