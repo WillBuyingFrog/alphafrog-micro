b115a3a5017d4e2932c73ff6fa648d8d3d936aac
Frog Weng <froggywch@gmail.com>
2026-01-02 19:51:06 +0800

    debug: set default mode to DEBUG

--- frontend/src/main/java/world/willfrog/alphafrogmicro/frontend/config/SecurityProperties.java
+++ frontend/src/main/java/world/willfrog/alphafrogmicro/frontend/config/SecurityProperties.java
@@ -2,9 +2,7 @@ package world.willfrog.alphafrogmicro.frontend.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "security")
 @Data
 public class SecurityProperties {
